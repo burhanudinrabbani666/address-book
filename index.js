175199ec005656f29c9343538ad5c5cafbae4ba1
burhanudinrabbani666 <burhanudinrabbani666@gmail.com>
2025-10-04 20:57:38 +0700

    testjavascript

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // alert("Hello, world!");
+
+// var, let, const
+// const message = "Hello World!";
+// window.alert(message);
+
+// const $ = 1;
+// const _ = 2;
+
+// alert($ + _);
+
+const COLOR_RED = "#F00";
+const COLOR_GREEN = "#0F0";
+const COLOR_BLUE = "#00F";
+const COLOR_ORANGE = "#FF7F00";
+
+let color = COLOR_ORANGE;
