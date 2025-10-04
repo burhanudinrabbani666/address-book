df0d0fe3de9500023c501af45ea3d3c26ffba9b5
burhanudinrabbani666 <burhanudinrabbani666@gmail.com>
2025-10-04 22:07:32 +0700

    tailwind aand icon

--- index.js
+++ index.js
@@ -9,9 +9,9 @@
 
 // alert($ + _);
 
-const COLOR_RED = "#F00";
-const COLOR_GREEN = "#0F0";
-const COLOR_BLUE = "#00F";
-const COLOR_ORANGE = "#FF7F00";
+// const COLOR_RED = "#F00";
+// const COLOR_GREEN = "#0F0";
+// const COLOR_BLUE = "#00F";
+// const COLOR_ORANGE = "#FF7F00";
 
-let color = COLOR_ORANGE;
+// let color = COLOR_ORANGE;
