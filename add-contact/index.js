abc03321a4a2415be648730853b9cc49db6191b3
burhanudinrabbani666 <burhanudinrabbani666@gmail.com>
2025-10-25 05:19:01 +0700

    newContactForm.get

--- add-contact/index.js
+++ add-contact/index.js
@@ -2,5 +2,15 @@ const addContactFormElement = document.getElementById("add-contact-form");
 
 addContactFormElement.addEventListener("submit", (event) => {
   event.preventDefault();
-  console.log(`sigma`);
+  const formData = new FormData(addContactFormElement);
+
+  const newContactData = {
+    fullName: formData.get("full-name").toString(),
+    phone: formData.get("phone").toString(),
+    email: formData.get("email").toString(),
+    company: formData.get("company").toString(),
+    birthdate: formData.get("birthdate").toString(),
+    address: formData.get("address").toString(),
+  };
+  console.log(newContactData);
 });
