31f9de79cdc2f63476dc2327ccd291666dd2a231
burhanudinrabbani666 <burhanudinrabbani666@gmail.com>
2025-10-24 22:32:38 +0700

    add contact  form handle element

---
+++ add-contact/index.js
@@ -0,0 +1,6 @@
+const addContactFormElement = document.getElementById("add-contact-form");
+
+addContactFormElement.addEventListener("submit", (event) => {
+  event.preventDefault();
+  console.log(`sigma`);
+});
